fbcb6736f5b5c6a2b4f34ababc5fb9fff185aaf4
cuong <cuongnguyen@b13technology.com>
2021-06-13 17:03:25 +0700

    init

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Page1(),
+    );
+  }
+}
+
+class Page1 extends StatefulWidget {
+  @override
+  _Page1State createState() => _Page1State();
+}
+
+class _Page1State extends State<Page1> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(runtimeType.toString()),
+      ),
+      body: Container(),
+    );
+  }
+}
